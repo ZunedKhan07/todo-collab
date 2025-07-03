f59977d5b2a7fe09ff66dfacd4925176e18aa344
Zuned khan <zunedkhan107@gmail.com>
2025-07-03 16:50:55 +0530

    Implement user authentication with registration and login functionality

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1/auth", authRoutes);
+
+connectDB();
+
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
